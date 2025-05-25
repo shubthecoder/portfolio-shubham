4c565aa678fa76818060ba36aa11eaf79c72eb30
Shubham Kumar Gupta <shubhamthe9@gmail.com>
2025-05-26 01:40:47 +0530

    Added footer

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function toggleMobileMenu() {
   document.getElementById("menu").classList.toggle("active");
 }
+
+function getDate() {
+  const year = new Date().getFullYear();
+  document.getElementById('year').textContent = year;
+}
